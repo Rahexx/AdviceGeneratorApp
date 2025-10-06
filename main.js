164360340484b151760e72152b453842cb5e69b8
Rahexx <endriu1297@gmail.com>
2025-10-06 09:17:23 +0200

    feat: add generating random advice when clicking on dive button

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const adviceNumber = document.querySelector('.advice__number');
 const adviceText = document.querySelector('.advice__text');
+const diceButton = document.querySelector('.advice__random--btn');
 
-window.addEventListener('DOMContentLoaded', () => {
-    fetch('https://api.adviceslip.com/advice')
+function handleFetchAdvice() {
+    const adviceId = Math.floor(Math.random() * 225) + 1;;
+    fetch(`https://api.adviceslip.com/advice/${adviceId}`)
     .then(res => res.json())
     .then(data => {
         adviceNumber.textContent = `${data.slip.id}`;
         adviceText.textContent = `"${data.slip.advice}"`;
     });
-});
+}
+
+window.addEventListener('DOMContentLoaded', handleFetchAdvice);
+diceButton.addEventListener('click', handleFetchAdvice);
